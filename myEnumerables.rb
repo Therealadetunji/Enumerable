67a241751a64d2abdf8a895464d6c7f979ed3b3a
Michael <madetunji1376@gmail.com>
2022-09-30 11:07:32 +0100

    myEnumerable and myList files created

---
+++ myEnumerables.rb
@@ -0,0 +1,21 @@
+module MyEnumerable
+	def all?
+		true unless block_given?
+    
+	 	    each { |n|  return false unless yield n }
+		true
+	end
+
+	   def any?
+	   	true unless block_given?
+     
+	    	 each { |n| return true if yield n } 
+	    false
+	   end
+  
+	   def filter
+	   	 elements = []
+	   	 each { |n| elements.push(n) if yield n }
+	     elements
+	   end
+end
